bf4d455c3f85705156a3838da463448424a4d721
grh-gitee <3494242908@qq.com>
2023-11-28 18:03:30 +0800

    feat: snippets

--- src/tree.ts
+++ src/tree.ts
@@ -1,55 +1,49 @@
 import * as vscode from 'vscode'
 
 const c1Tree = {
-  'Snippets': {
-    'vue': {
-      '!js': 'script-template-js',
-      '!ts': 'script-template-ts',
-    }
-  }
+  // 'Snippets': {
+  'vue  !js': 'script-template-js',
+  'vue  !ts': 'script-template-ts',
+  // }
 }
-
-const aNodeWithIdTreeDataProvider = {
-  getTreeItem: (el: any) => {
-    let treeItem = {}
-    if (typeof el === 'string') {
-      treeItem = {
-        label: el,
-        collapsibleState: 0,
-        tooltip: "hover: 单纯的字符串 ",
-        // id: new Date().getTime()
-      }
-    } else {
-      treeItem = {
-        label: el._cc_key,
-        collapsibleState: 1,
-        tooltip: "hover: 可展开 ",
-        // id: new Date().getTime()
-      }
-    }
-    return treeItem
-  },
-  getChildren: (el: any) => {
-    const arr = []
-    const tree = el || c1Tree
-    for (let item in tree) {
-      const activeItem = tree[item]
-      if (typeof activeItem !== 'object') {
-        arr.push(`${item}：${activeItem}`)
-      } else {
-        Object.defineProperty(activeItem, "_cc_key", {
-          get: function () { return item },
-          enumerable: false
-        })
-        arr.push(activeItem)
-      }
-    }
-    return arr
-  },
-}
-
 
 export const tree = vscode.window.createTreeView('c1', {
-  treeDataProvider: aNodeWithIdTreeDataProvider,
+  treeDataProvider: {
+    getTreeItem: (el: any) => {
+      let treeItem = {}
+      if (typeof el === 'string') {
+        treeItem = {
+          label: el,
+          collapsibleState: 0,
+          tooltip: "hover: 单纯的字符串 ",
+        }
+      } else {
+        treeItem = {
+          label: el._cc_key,
+          collapsibleState: 1,
+          // tooltip: "hover: 可展开 ",
+          // tooltip: JSON.stringify(vueJson, null, 2),
+        }
+      }
+      return treeItem
+    },
+    getChildren: (el: any) => {
+      const arr = []
+      const tree = el || c1Tree
+      for (let item in tree) {
+        const activeItem = tree[item]
+        if (typeof activeItem !== 'object') {
+          arr.push(`${item}：${activeItem}`)
+        } else {
+          Object.defineProperty(activeItem, "_cc_key", {
+            get: function () { return item },
+            enumerable: false
+          })
+          arr.push(activeItem)
+        }
+      }
+      return arr
+    },
+  },
   showCollapseAll: true,
 })
